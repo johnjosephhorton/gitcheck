7e86bd36cec7060d95d7e98600e18e958094f75f
John Horton <john.joseph.horton@gmail.com>
2013-07-23 11:02:04 -0700

    Add distirbution capability

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from os.path import join, dirname
+
+import gitcheck
+
+setup(name='gitcheck',
+      version = gitcheck.__version__,
+      author = gitcheck.__author__ , 
+      author_email = gitcheck.__email__,
+      url = 'http://github.com/johnjosephhorton/gitcheck',
+      packages = [''],
+      package_data = {'':['*.md', 'checklists/*.md']},
+      package_dir= {'':'.'}, 
+      entry_points={
+          'console_scripts':
+              ['gitcheck = gitcheck:main',
+               ]}, 
+      classifiers=(
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'License :: OSI Approved :: GNU General Public License v3 or '
+          'later (GPLv3+)',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ),
+      install_requires=['Jinja2>=2.6'],
+      )
+
